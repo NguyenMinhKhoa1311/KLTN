6be2bb7d38e51b27e74bf2fbb1edf5eb5d0c4975
minhkhoa1311 <khoa.nm00716@sinhvien.hoasen.edu.vn>
2024-03-12 13:05:28 +0700

    update server

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://nguyenminhkhoa1311:cn4ABLLOwrbH4m4z@cluster0.bj8qqej.mongodb.net/'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
