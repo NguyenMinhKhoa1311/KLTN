4607156dd8de156c59214e1d4f80c4caabd55822
PhKhoa <khoa.pm14576@sinhvien.hoasen.edu.vn>
2024-03-23 14:56:58 +0700

    pull

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -7,21 +7,23 @@ export const routes: Routes = [
         pathMatch: 'full',
     },
     {
-    path: '',
-    loadChildren: () =>
-        import('./pages/layout/layout.routes').then((m) => m.LAYOUT_ROUTERS),
+        path: '',
+        loadChildren: () =>
+            import('./pages/layout/layout.routes').then((m) => m.LAYOUT_ROUTERS),
     },
     {
-    path: 'login',
-    loadChildren: () =>
-        import('./pages/login/login.routes').then((m) => m.LOGIN_ROUTERS),
+        path: 'login',
+        loadChildren: () =>
+            import('./pages/login/login.routes').then((m) => m.LOGIN_ROUTERS),
     },
     {
         path: 'register',
         loadChildren: () =>
-          import('./pages/register/register.routes').then(
-            (m) => m.REGISTER_ROUTERS,
-          ),
+          import('./pages/register/register.routes').then((m) => m.REGISTER_ROUTERS),
     },  
-      
+    {
+        path: 'createProfile',
+        loadChildren: () =>
+          import('./pages/createProfile/create-profile.routes').then((m) => m.CREATEPROFILE_ROUTERS),
+    }, 
 ];
