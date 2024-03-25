8cb3df0959ea3d072cb93377e5277ced53f9a376
PhKhoa <khoa.pm14576@sinhvien.hoasen.edu.vn>
2024-03-25 13:23:30 +0700

    push

--- client/src/app/pages/layout/job/job.component.ts
+++ client/src/app/pages/layout/job/job.component.ts
@@ -1,12 +1,116 @@
-import { Component } from '@angular/core';
+import { Component,ChangeDetectionStrategy } from '@angular/core';
+import { ShareModule } from '../../../shared/shared.module';
+import { TaigaModule } from '../../../shared/taiga.module';
+import { Store } from '@ngrx/store';
+import { jobState } from '../../../ngrx/states/job.state';
+import * as JobActions from '../../../ngrx/actions/job.actions';
 
 @Component({
   selector: 'app-job',
   standalone: true,
-  imports: [],
+  imports: [ShareModule,TaigaModule],
   templateUrl: './job.component.html',
-  styleUrl: './job.component.less'
+  styleUrl: './job.component.less',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class JobComponent {
 
+  page: number = 0;
+  throttle = 500;
+  scrollDistance = 1;
+  scrollUpDistance = 2;
+
+  onScrollDown(ev: any) {
+    console.log('scrolled down!!', ev);
+    this.page += 1;
+    // this.store.dispatch(
+    //   JobActions.getByFieldAtHome({ page: this.page, pageSize: 2 })
+    // );
+  }
+
+  constructor(
+    private store: Store<{ job: jobState }>
+  ){}
+
+
+  readonly locations = [
+      'Tất cả địa điểm',
+      'Ninh Thuận',
+      'Ninh Bình',
+      'Bình Dương',
+      'Bình Chánh',
+      'Hồ Chí Minh',
+      'Phan Thiết',
+  ];
+  locationValue = 'Tất cả địa điểm';
+
+  readonly industries = [
+      'Tất cả loại công việc',
+      'Full-time',
+      'Part-time',
+      'Thực tập',
+      'Remote',
+  ];
+  industryValue = 'Tất cả loại công việc';
+
+  readonly fields = [
+    'Tất cả loại công việc',
+    'Full-time',
+    'Part-time',
+    'Thực tập',
+    'Remote',
+  ];
+  fieldValue = 'Tất cả loại công việc';
+ 
+  readonly bestWorkList = [
+    {
+      nameWork: 'Truong phong kinh doanh kinh te',
+      company: 'Google',
+      location: '1',
+    },
+    {
+      nameWork: 'Truong phong kinh qweqweqweqweqwe',
+      company: 'Google',
+      location: '2',
+    },
+    {
+      nameWork: 'Truong phong kinh danh kinh te',
+      company: 'Google',
+      location: '3',
+    },
+    {
+      nameWork: 'Truong phong kinh doanh kinh te',
+      company: 'Google',
+      location: '4',
+    },
+    {
+      nameWork: 'Truong phong kinh qweqweqweqweqwe',
+      company: 'Google',
+      location: '5',
+    },
+    {
+      nameWork: 'Truong phong kinh danh kinh te',
+      company: 'Google',
+      location: '6',
+    },
+    {
+      nameWork: 'Truong phong kinh doanh kinh te',
+      company: 'Google',
+      location: '7',
+    },
+    {
+      nameWork: 'Truong phong kinh doanh kinh te',
+      company: 'Google',
+      location: '8',
+    },
+    {
+      nameWork: 'Truong phong kinh doanh kinh te',
+      company: 'Google',
+      location: '9',
+    },
+    
+   
+    
+    
+  ];
 }
--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
+
 
 @NgModule({
-  imports: [CommonModule, FormsModule, ReactiveFormsModule, HttpClientModule],
-  exports: [CommonModule, FormsModule, ReactiveFormsModule, HttpClientModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, HttpClientModule, InfiniteScrollModule ],
+  exports: [CommonModule, FormsModule, ReactiveFormsModule, HttpClientModule,InfiniteScrollModule ],
 })
 export class ShareModule {}
