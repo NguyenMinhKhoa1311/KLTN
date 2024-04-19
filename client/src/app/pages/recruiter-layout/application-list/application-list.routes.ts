990dc7a1979a45902545deac27f3823e82c18516
PhKhoa <khoa.pm14576@sinhvien.hoasen.edu.vn>
2024-04-20 00:46:08 +0700

    push

---
+++ client/src/app/pages/recruiter-layout/application-list/application-list.routes.ts
@@ -0,0 +1,9 @@
+import { ApplicationListComponent } from './application-list.component';
+import { Routes } from '@angular/router';
+
+export const APPLICATIONLIST_ROUTERS: Routes = [
+  {
+    path: '',
+    component: ApplicationListComponent,
+  },
+];
