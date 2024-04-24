581a2b2bd2c4d87f024e3dd7104d305ea68169fa
PhKhoa <khoa.pm14576@sinhvien.hoasen.edu.vn>
2024-04-24 21:21:33 +0700

    push

--- client/src/app/pages/layout/job-detail/job-detail.component.ts
+++ client/src/app/pages/layout/job-detail/job-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy } from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef, OnDestroy, ViewChild, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { jobState } from '../../../ngrx/states/job.state';
@@ -6,11 +6,12 @@ import * as JobActions from '../../../ngrx/actions/job.actions';
 import { Subscription } from 'rxjs';
 import { Job } from '../../../models/job.model';
 import { ShareModule } from '../../../shared/shared.module';
+import { TaigaModule } from '../../../shared/taiga.module';
 
 @Component({
   selector: 'app-job-detail',
   standalone: true,
-  imports: [ShareModule],
+  imports: [ShareModule,TaigaModule],
   templateUrl: './job-detail.component.html',
   styleUrl: './job-detail.component.less'
 })
@@ -25,8 +26,6 @@ export class JobDetailComponent implements OnDestroy {
 
   //ngrx of jobs
   jobTakenByIdAtJobDetailOfCandidate$ = this.store.select('job', 'jobTakenByIdAtJobDetailOfCandidate');
-
-
 
 
   constructor(private route: ActivatedRoute,  private store : Store<{job: jobState}>,) {
@@ -52,6 +51,18 @@ export class JobDetailComponent implements OnDestroy {
     this.subscriptions.forEach((subscription) => {
       subscription.unsubscribe();
     });
+  }
+
+  @ViewChild('applyDialog', { static: true })
+  applyDialog!: ElementRef<HTMLDialogElement>;
+  cdr1 = inject(ChangeDetectorRef);
+  openApplyDialog() {
+    this.applyDialog.nativeElement.showModal();
+    this.cdr1.detectChanges();
+  }
+  closeApplyDialog() {
+    this.applyDialog.nativeElement.close();
+    this.cdr1.detectChanges();
   }
 
 
