12e9ddc0405badd1d69c991f4d79b4ebdc073f9d
PhKhoa <khoa.pm14576@sinhvien.hoasen.edu.vn>
2024-05-21 21:44:38 +0700

    push

---
+++ client/src/app/pages/recruiter-layout/payment/payment.routes.ts
@@ -0,0 +1,9 @@
+import { PaymentComponent } from './payment.component';
+import { Routes } from '@angular/router';
+
+export const PAYMENT_ROUTERS: Routes = [
+  {
+    path: '',
+    component: PaymentComponent,
+  },
+];
